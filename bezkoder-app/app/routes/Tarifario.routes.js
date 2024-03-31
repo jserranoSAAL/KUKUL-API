2e32145e7a4d8f521067cd7e6c5e108df9cc7305
jserranoSAAL <jorge.serrano4502@gmail.com>
2024-03-30 22:13:24 -0600

    authentication middleware api Tarifarios

--- bezkoder-app/app/routes/Tarifario.routes.js
+++ bezkoder-app/app/routes/Tarifario.routes.js
@@ -2,22 +2,23 @@ module.exports = app => {
     const tarifarioController = require("../controllers/Tarifario.controller.js");
 
     const tarifarioRouter = require("express").Router();
+    const { requireAuth } = require("../middlewares/auth");
 
     // Ruta para crear un nuevo Gasto
-    tarifarioRouter.post("/", tarifarioController.create);
+    tarifarioRouter.post("/", requireAuth, tarifarioController.create);
 
     // Ruta para recuperar todos los Gastos
-    tarifarioRouter.get("/", tarifarioController.findAll);
+    tarifarioRouter.get("/", requireAuth, tarifarioController.findAll);
 
     // Ruta para recuperar un único Gasto con id
-    tarifarioRouter.get("/:id", tarifarioController.findOne);
+    tarifarioRouter.get("/:id", requireAuth, tarifarioController.findOne);
 
     // Ruta para actualizar un Gasto con id
-    tarifarioRouter.put("/:id", tarifarioController.update);
+    tarifarioRouter.put("/:id", requireAuth, tarifarioController.update);
 
     // Ruta para eliminar un Gasto con id
-    tarifarioRouter.delete("/:id", tarifarioController.delete);
+    tarifarioRouter.delete("/:id", requireAuth, tarifarioController.delete);
 
     // Montar el enrutador bajo la ruta /api/gastos
-    app.use("/api/tarifario", tarifarioRouter);
+    app.use("/api/tarifario", requireAuth, tarifarioRouter);
 };
