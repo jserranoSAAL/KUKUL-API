c338bd61885525660bbfdd5633ec4755f9397e47
jserranoSAAL <jorge.serrano4502@gmail.com>
2024-03-31 16:09:28 -0600

    metodo de autenticacion en creditos y debitos

--- bezkoder-app/app/routes/CreditosDebitos.routes.js
+++ bezkoder-app/app/routes/CreditosDebitos.routes.js
@@ -2,21 +2,22 @@ module.exports = app => {
     const creditosDebitosController = require("../controllers/CreditosDebitos.controller.js");
 
     const creditosDebitosRouter = require("express").Router();
+    const { requireAuth } = require("../middlewares/auth");
 
     // Ruta para crear un nuevo registro de Créditos o Débitos
-    creditosDebitosRouter.post("/", creditosDebitosController.create);
+    creditosDebitosRouter.post("/", requireAuth, creditosDebitosController.create);
 
     // Ruta para recuperar todos los registros de Créditos y Débitos
-    creditosDebitosRouter.get("/", creditosDebitosController.findAll);
+    creditosDebitosRouter.get("/", requireAuth, creditosDebitosController.findAll);
 
     // Ruta para recuperar un único registro de Créditos o Débitos con id
-    creditosDebitosRouter.get("/:id", creditosDebitosController.findOne);
+    creditosDebitosRouter.get("/:id", requireAuth, creditosDebitosController.findOne);
 
     // Ruta para actualizar un registro de Créditos o Débitos con id
-    creditosDebitosRouter.put("/:id", creditosDebitosController.update);
+    creditosDebitosRouter.put("/:id", requireAuth, creditosDebitosController.update);
 
     // Ruta para eliminar un registro de Créditos o Débitos con id
-    creditosDebitosRouter.delete("/:id", creditosDebitosController.delete);
+    creditosDebitosRouter.delete("/:id", requireAuth, creditosDebitosController.delete);
 
     // Montar el enrutador bajo la ruta /api/creditosDebitos
     app.use("/api/creditosDebitos", creditosDebitosRouter);
