ec810c64e96d46be492bb3102b505cd7cfb13795
jserranoSAAL <jorge.serrano4502@gmail.com>
2024-03-31 16:25:56 -0600

    metodo de autenticacion en facturas

--- bezkoder-app/app/routes/Facturas.routes.js
+++ bezkoder-app/app/routes/Facturas.routes.js
@@ -2,21 +2,22 @@ module.exports = app => {
     const facturasController = require("../controllers/Facturas.controller.js");
 
     const facturasRouter = require("express").Router();
+    const { requireAuth } = require("../middlewares/auth");
 
     // Ruta para crear una nueva Factura
-    facturasRouter.post("/", facturasController.create);
+    facturasRouter.post("/", requireAuth, facturasController.create);
 
     // Ruta para recuperar todas las Facturas
-    facturasRouter.get("/", facturasController.findAll);
+    facturasRouter.get("/", requireAuth, facturasController.findAll);
 
     // Ruta para recuperar una única Factura con id
-    facturasRouter.get("/:id", facturasController.findOne);
+    facturasRouter.get("/:id", requireAuth, facturasController.findOne);
 
     // Ruta para actualizar una Factura con id
-    facturasRouter.put("/:id", facturasController.update);
+    facturasRouter.put("/:id", requireAuth, facturasController.update);
 
     // Ruta para eliminar una Factura con id
-    facturasRouter.delete("/:id", facturasController.delete);
+    facturasRouter.delete("/:id", requireAuth, facturasController.delete);
 
     // Montar el enrutador bajo la ruta /api/facturas
     app.use("/api/facturas", facturasRouter);
