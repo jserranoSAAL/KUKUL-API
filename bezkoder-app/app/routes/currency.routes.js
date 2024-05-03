e75a59755f4b1aaf82c1700d5b2033610e698d9e
jserranoSAAL <jorge.serrano4502@gmail.com>
2024-05-03 10:51:03 -0600

    Manejo de currencys

--- bezkoder-app/app/routes/currency.routes.js
+++ bezkoder-app/app/routes/currency.routes.js
@@ -1,27 +1,27 @@
 // routes/currencyRoutes.js
 module.exports = app => {
     const currencies = require('../controllers/currency.controller.js');
+    const requireAuth = require('../middlewares/auth').requireAuth; // Asegúrate de que la ruta del middleware es correcta
   
     var router = require('express').Router();
   
-    // Create a new Currency
-    router.post('/', currencies.create);
+    // Create a new Currency - Protected
+    router.post('/', requireAuth, currencies.create);
   
-    // Retrieve all Currencies
-    router.get('/', currencies.findAll);
+    // Retrieve all Currencies - Protected
+    router.get('/', requireAuth, currencies.findAll);
   
-    // Retrieve a single Currency with id
-    router.get('/:id', currencies.findOne);
+    // Retrieve a single Currency with id - Protected
+    router.get('/:id', requireAuth, currencies.findOne);
   
-    // Update a Currency with id
-    router.put('/:id', currencies.update);
+    // Update a Currency with id - Protected
+    router.put('/:id', requireAuth, currencies.update);
   
-    // Delete a Currency with id
-    router.delete('/:id', currencies.delete);
+    // Delete a Currency with id - Protected
+    router.delete('/:id', requireAuth, currencies.delete);
   
-    // Set default Currency
-    router.patch('/set-default/:id', currencies.setDefault);
+    // Set default Currency - Protected
+    router.patch('/set-default/:id', requireAuth, currencies.setDefault);
   
     app.use('/api/currencies', router);
-  };
-  
+};
